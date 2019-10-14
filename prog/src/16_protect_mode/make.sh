c013276321eb23a2aaf54493e7034507c6f21495
ry3s <ktchg752@yahoo.co.jp>
2019-10-14 16:50:13 +0900

    finish protected mode

---
+++ prog/src/16_protect_mode/make.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+if [ -e kernel.s ]; then
+    nasm boot.s -o boot.bin -l boot.lst
+    nasm kernel.s -o kernel.bin -l kernel.lst
+    cat boot.bin kernel.bin > boot.img
+else
+    nasm boot.s -o boot.img -l boot.lst
+fi
